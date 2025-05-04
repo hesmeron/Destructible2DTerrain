64cb28a4d8f40ee2238c531000fa14e407e4f479
KajetanRadulski <hesmeron@gmail.com>
2025-05-04 14:29:51 +0200

    Cleanup

--- Assets/RenderFeature/DrawFeature/DrawPass.cs
+++ Assets/RenderFeature/DrawFeature/DrawPass.cs
@@ -1,33 +1,43 @@
+using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.RenderGraphModule;
 using UnityEngine.Rendering.Universal;
 
 class DrawPass : ScriptableRenderPass
 {
-    // This class stores the data needed by the RenderGraph pass.
-    // It is passed as a parameter to the delegate function that executes the RenderGraph pass.
+    private Material _material;
+    private Mesh _mesh;
+
+    public DrawPass(Material material, Mesh mesh)
+    {
+        _material = material;
+        _mesh = mesh;
+    }
+
     private class PassData
     {
+        public Material material;
+        public Mesh mesh;
     }
-
-    // This static method is passed as the RenderFunc delegate to the RenderGraph render pass.
-    // It is used to execute draw commands.
+    
     static void ExecutePass(PassData data, RasterGraphContext context)
     {
-        
-    }
+        int shaderPass = data.material.FindPass("ForwardLit");
+        context.cmd.DrawMesh(data.mesh, Matrix4x4.identity, data.material, 0, shaderPass);
 
-    // RecordRenderGraph is where the RenderGraph handle can be accessed, through which render passes can be added to the graph.
-    // FrameData is a context container through which URP resources can be accessed and managed.
+    }
+    
     public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
     {
         const string passName = "Render Custom Pass";
             
         using (var builder = renderGraph.AddRasterRenderPass<PassData>(passName, out var passData))
         {
-
+            passData.material = _material;
+            passData.mesh = _mesh;
             UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
             builder.SetRenderAttachment(resourceData.activeColorTexture, 0);
+            builder.SetRenderAttachmentDepth(resourceData.activeDepthTexture);
             builder.SetRenderFunc((PassData data, RasterGraphContext context) => ExecutePass(data, context));
         }
     }
--- Assets/RenderFeature/DrawFeature/ProceduralDrawFeature.cs
+++ Assets/RenderFeature/DrawFeature/ProceduralDrawFeature.cs
@@ -5,18 +5,21 @@ using UnityEngine.Rendering.RenderGraphModule;
 
 public class ProceduralDrawFeature : ScriptableRendererFeature
 {
-
-    DrawPass m_ScriptablePass;
+    [SerializeField] 
+    private Material material;
+    [SerializeField]
+    private Mesh mesh;
+    DrawPass renderPass;
 
     /// <inheritdoc/>
     public override void Create()
     {
-        m_ScriptablePass = new DrawPass();
-        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
+        renderPass = new DrawPass(material, mesh);
+        renderPass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
     }
     
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
-        renderer.EnqueuePass(m_ScriptablePass);
+        renderer.EnqueuePass(renderPass);
     }
 }
