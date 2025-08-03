8e2a0402989d83ffe51ca224364556f4c2581a1c
KajetanRadulski <hesmeron@gmail.com>
2025-08-03 14:05:59 +0200

    cleanup

--- Assets/GPUInstancing/Scripts/RenderFeature/InstancedDrawSystem.cs
+++ Assets/GPUInstancing/Scripts/RenderFeature/InstancedDrawSystem.cs
@@ -6,29 +6,13 @@ public static class InstancedDrawSystem
     private static GraphicsBuffer _matrixBuffer;
     public static GraphicsBuffer GetMatrixBuffer()
     {
-        
         if (_matrixBuffer == null)
         {
-            Matrix4x4[] matrices = new Matrix4x4[100 * 100];
-        /*
-            for (int x = 0; x < 100; x++)
-            {
-                for (int z = 0; z < 100; z++)
-                {
-                    Matrix4x4 matrix = Matrix4x4.TRS(new Vector3(x*1.2f, 0, z*1.5f), Quaternion.identity, Vector3.one);
-                    matrices[x * 100 + z] = matrix;
-                }
-            }
-            */
-
             _matrixBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured 
                                                | GraphicsBuffer.Target.Append, 
                                         10000, sizeof(int));
-            //_matrixBuffer.SetCounterValue((uint) matrices.Length);
             _matrixBuffer.name = "MatrixBuffer";
         }
-        //_matrixBuffer.SetData(Array.Empty<int>());
-
         return _matrixBuffer;
     }    
     
